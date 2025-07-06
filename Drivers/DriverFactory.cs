a7e2c37eb3a7ca255988cb6e9e78deb8d901a131
ISRG <shubhamgiri602@gmail.com>
2025-07-06 21:46:28 +0530

    Update DriverFactory.cs

--- Drivers/DriverFactory.cs
+++ Drivers/DriverFactory.cs
@@ -1,14 +1,35 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SeleniumSpecflowFramework.Helpers;
 
 namespace SeleniumSpecflowFramework.Drivers
 {
-    public static class DriverFactory
+    public class BrowserDriver
     {
-        public static IWebDriver CreateDriver()
+        public IWebDriver Current { get; private set; }
+
+        public BrowserDriver()
         {
-            var options = new ChromeOptions();
-            return new ChromeDriver(options);
+            string browser = ResxReader.Get("Browser").ToLower();
+            int implicitWait = ResxReader.GetInt("ImplicitWaitInSeconds");
+
+            if (browser == "chrome")
+            {
+                var options = new ChromeOptions();
+                options.AddArgument("--start-maximized");
+                Current = new ChromeDriver(options);
+            }
+            else
+            {
+                throw new NotSupportedException($"Browser '{browser}' is not supported.");
+            }
+
+            Current.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(implicitWait);
+        }
+
+        public void Quit()
+        {
+            Current.Quit();
         }
     }
 }
